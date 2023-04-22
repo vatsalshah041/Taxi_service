746c2ff90d7df8d1b400c7a40d9ec290a4feec7f
vatsalshah041 <shahvatsal2003@gmail.com>
2023-04-22 17:37:02 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    
+        <Navbar></Navbar>
         <Form></Form>
 
     </>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,24 @@
-import { Grid, Paper, TextField, Typography } from '@mui/material'
+import { Button, Grid, Paper, TextField, Typography } from '@mui/material'
 import Pin from '@mui/icons-material/PushPinRounded';
 import React, { useEffect, useState } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 
+import MyFormControlLabel from '@mui/material/FormControlLabel';
 
 export default function () {
   const [currentCity, setCurrentCity] = useState(null);
+  const [pick,setPick]=useState();
+  const [drop,setDrop]=useState();
+  const [sc,setSc]=useState();
+  //const [info,setInfo]=useState([false,false,false,false]);
+  const [infoc,setInfoc]=useState([0,0,0,0]);
+  const [add,setAdd]=useState();
+
+  const submit=()=>
+  {
+
+  }
 
   const getloc = () => {
     navigator.geolocation.getCurrentPosition(
@@ -31,7 +40,76 @@ export default function () {
       })
   }
   //getloc();
+  const handlepick=(e)=>{
+    setPick(e.target.value);
+    console.log(pick);
+  }
+  const handledrop=(e)=>{
+    setDrop(e.target.value);
+    console.log(drop);
+  }
+  const book=(e)=>{
+    setSc(e.target.value);
+    console.log(sc);
+  }
+  const handlei=(e)=>{
+    let x=e.target.value
+    
+    if(x=='baby')
+    {
+      if(infoc[0]==0)
+      {
+        infoc[0]=1;
+      }
+      else
+      {
+        infoc[0]=0;
+      }
+    }
+    else if(x=='carrier')
+    {
+      if(infoc[1]==0)
+      {
+        infoc[1]=1;
+      }
+      else
+      {
+        infoc[1]=0;
+      }
+    }
+    else if(x=='chair')
+    {
+      if(infoc[2]==0)
+      {
+        infoc[2]=1;
+      }
+      else
+      {
+        infoc[2]=0;
+      }
+    }
+    else if(x=='trunk')
+    {
+      if(infoc[3]==0)
+      {
+        infoc[3]=1;
+      }
+      else
+      {
+        infoc[3]=0;
+      }
+    }
+    for(let i=0;i<4;i++)
+    {
+      console.log(infoc[i]);
+    }
 
+    console.log(e.target.value);
+  }
+  const handleadd=(e)=>{
+    setAdd(e.target.value)
+    console.log((add));
+  }
   return (
     <>
 
@@ -50,6 +128,8 @@ export default function () {
                   label="Pickup Location"
                   multiline
                   sx={{ width: '430px' }}
+                  value={pick}
+                  onChange={handlepick}
                 />
               </Grid>
               <Grid item md={0.7}></Grid>
@@ -59,32 +139,59 @@ export default function () {
                   id="outlined-multiline-flexible"
                   label="Drop Location"
                   multiline
-                  sx={{ width: '430px'}}
+                  sx={{ width: '430px' }}
+                  value={drop}
+                  onChange={handledrop}
                 />
               </Grid>
-              <Grid item md={2}></Grid>
-              <Grid item md={5} >
-                <input type='radio' value='now'/><b>Now</b>
+              <Grid item md={1}></Grid>
+              <Grid item md={5} sx={{ backgroundColor: '#ff9800d6', borderRadius: '40px', paddingBottom: '24px', marginTop: '10px' }}>
+                <input type="radio" id="html" name="fav_language" value="now" onClick={book}/>
+                <label for="html"><Typography variant='h5'>Now</Typography></label>
               </Grid>
-              <Grid item md={5}>
-              <input type='radio' value='later'/><b>Schedule</b>
+              <Grid md={0.5}></Grid>
+              <Grid item md={5} sx={{ backgroundColor: '#ff9800d6', borderRadius: '40px', paddingBottom: '24px', marginTop: '10px' }}>
+                <input type="radio" id="css" name="fav_language" value="later" onClick={book}/>
+                <label for="css"><Typography variant='h5'>Schedule Later</Typography></label>
               </Grid>
+              <Grid md={0.5}></Grid>
+              <hr></hr>
               <Grid item md={0.8}></Grid>
               <Grid item md={5}>
-                <input type='checkbox'/>Baby Seat
+                <input type='checkbox' value='baby' onClick={handlei}/>Baby Seat
               </Grid>
               <Grid item md={5}>
-                <input type='checkbox'/>Carrier
+                <input type='checkbox' value='carrier' onClick={handlei}/>Carrier
               </Grid>
               <Grid item md={1.2}></Grid>
               <Grid item md={0.8}></Grid>
               <Grid item md={5}>
-                <input type='checkbox' label='Wheel Chair'/>Wheel Chair
+                <input type='checkbox' value='chair' onClick={handlei}/>Wheel Chair
               </Grid>
               <Grid item md={5}>
-                <input type='checkbox'/>Trunk
+                <input type='checkbox' value='trunk' onClick={handlei}/>Trunk
               </Grid>
               <Grid item md={1.2}></Grid>
+              <hr></hr>
+              <Grid item md={0.8}></Grid>
+              <Grid item md={11.2}>
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Additonal Information"
+                  multiline
+                  rows={2}
+                  //placeholder='Enter text here...'
+                  sx={{ width: '430px' }}
+                  value={add}
+                  onChange={handleadd}
+                />
+              </Grid>
+              <br></br>
+              <br></br>
+              <Grid item md={0.8}></Grid>
+              <Grid item md={4}>
+              </Grid>
+              <Button variant='filled' onClick={submit}>Submit</Button>
             </Grid>
           </Paper>
 
